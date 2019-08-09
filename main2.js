7f9a1efab96305c33b43fafc247830dad24ba547
bahubali <bahubalirockz@gmail.com>
2019-08-09 16:20:09 +0530

    changed main2.js

--- main2.js
+++ main2.js
@@ -1,29 +1,3 @@
-const ul = document.querySelector('.items');
-//ul.remove();
-
-//ul.lastElementChild.remove();
-
-/*
-ul.firstElementChild.textContent = 'hello';
-ul.children[1].innerText = 'bahubali';
-ul.lastElementChild.innerHTML = '<h1>akshay</h1>';
-
-const btn = document.querySelector('.btn');
-btn.style.background = 'red';
-
-
-const btn = document.querySelector('.btn');
-
-btn.addEventListener('mouseout', (e) => {
-    e.preventDefault();
-    document.querySelector('#my-form').style.background = '#ccc';
-    document.querySelector('body').classList.add('bg-dark');
-    document.querySelector('.items').lastElementChild.innerHTML = '<h1>hello</h1>';
-});
-
-*/
-
-
 const myform = document.querySelector('#my-form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
